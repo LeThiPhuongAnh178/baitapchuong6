7a4931c3ad866c67119c15684d6eff56fea4d761
LeThiPhuongAnh178 <phuonganhlethi43@gmail.com>
2024-08-25 17:47:45 +0700

    Add files via upload

---
+++ baitapchuoi1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Phần tách thứ 1: ")
+
+var1 = 'Đại học Quốc Gia'
+print("Thành var1[0:16] ", var1[0:16])
+var2 = 'Khu phố 6'
+print("Thành var2[0:9]", var2[0:9])
+var3 = 'P.Linh Trung'
+print("Thành var3[0:12]",var3[0:12])
+var4 = 'Q.Thủ Đức'
+print("Thành var4[0:9]",var4[0:9])
+var5 = 'Tp.HCM'
+print("Thành var5[0:6]",var5[0:6])
+
+print(" Phần tách thứ 2: ")
+    
+var1 = 'Đại học Quốc Gia'
+print("Thành var1[0:16] ", var1[0:16])
+var2 = 'Khu phố 6'
+print("Thành var2[0:9]", var2[0:9])
+var3 = 'P.Linh Trung'
+print("Thành var3[2:12]",var3[2:12])
+var4 = 'Q.Thủ Đức'
+print("Thành var4[2:9]",var4[2:9])
+var5 = 'Tp.HCM'
+print("Thành var5[3:6]",var5[3:6])
---
+++ baitapchuoi2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 17:45:43 2024
+
+@author: ACER
+"""
+
+var1= "i'm a cat"
+var2= "I'm A Cat"
+
+print(var1.title())
+
+print(var1.upper())
+
+print(var2.swapcase())
+
+print(var1.capitalize())
